e79f1085447f4f8f48be143ff2794e2da9e764e8
ashleymccrayspeaks-cloud <ashley.mccray.speaks@gmail.com>
2026-04-02 15:25:05 -0500

    Update analyze.js

--- netlify/functions/analyze.js
+++ netlify/functions/analyze.js
@@ -1,8 +1,20 @@
 exports.handler = async (event) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+      body: ''
+    };
+  }
+
   try {
     const body = JSON.parse(event.body);
     const prompt = body.prompt;
-    
+
     const res = await fetch('https://api.anthropic.com/v1/messages', {
       method: 'POST',
       headers: {
@@ -18,15 +30,19 @@ exports.handler = async (event) => {
     });
 
     const data = await res.json();
-    
+
     return {
       statusCode: 200,
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
       body: JSON.stringify(data)
     };
   } catch (err) {
     return {
       statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
       body: JSON.stringify({ error: err.message })
     };
   }
